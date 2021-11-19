b887fbc97fc172b3c7c0f69c1af53e8025e483fb
Juani Gerarduzzi <jigerarduzzi@gmail.com>
2021-11-19 11:26:24 -0800

    allow button optimization

--- clasificador.py
+++ clasificador.py
@@ -1,8 +1,17 @@
+import os
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome(r'C:/Users/Juani Gerarduzzi/Desktop/UTN/PROYECTO/code/chromedriver.exe')
+chromedriver = 'C:/Users/Juani Gerarduzzi/Desktop/UTN/PROYECTO/code/chromedriver.exe'
 
 URL = 'https://www.boletinoficial.gob.ar/seccion/primera'
+os.environ["webdriver.chrome.driver"] = chromedriver
+chrome_options = Options()
+
+prefs = {'profile.default_content_setting_values.automatic_downloads': 1}
+chrome_options.add_experimental_option("prefs", prefs)
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
 driver.get(URL)
 driver.execute_script("descargarPDFSeccion('primera','20211117', '/pdf/download_section')")
 driver.execute_script("descargarPDFSeccion('primera','20211116', '/pdf/download_section')")
