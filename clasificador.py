80bc31463f9dfce828bbcf4c24c5816c7b680302
Juani Gerarduzzi <jigerarduzzi@gmail.com>
2021-11-19 08:45:04 -0800

    advances

--- clasificador.py
+++ clasificador.py
@@ -1,17 +1,8 @@
-import os
 import requests
 
+URL = "https://www.boletinoficial.gob.ar/seccion/primera"
+page = requests.get(URL)
+
+print(page.text)
 
 
-urls = [
-    'http://s3.arsat.com.ar/cdn-bo-001/pdf-del-dia/primera.pdf',
-]
-
-output_dir = '.\output'
-
-for url in urls:
-    response = requests.get(url)
-    if response.status_code==200:
-        file_path = os.path.join(output_dir,os.path.basename(url))
-        with open (file_path,'wb') as f:
-            f.write(response.content)
