0e6fe4ac7533b8956a0ae7f2303369949e2f8f80
Juani Gerarduzzi <jigerarduzzi@gmail.com>
2021-11-19 11:19:07 -0800

    finished

--- clasificador.py
+++ clasificador.py
@@ -1,8 +1,12 @@
-import requests
+from selenium import webdriver
 
-URL = "https://www.boletinoficial.gob.ar/seccion/primera"
-page = requests.get(URL)
+driver = webdriver.Chrome(r'C:/Users/Juani Gerarduzzi/Desktop/UTN/PROYECTO/code/chromedriver.exe')
 
-print(page.text)
+URL = 'https://www.boletinoficial.gob.ar/seccion/primera'
+driver.get(URL)
+driver.execute_script("descargarPDFSeccion('primera','20211117', '/pdf/download_section')")
+driver.execute_script("descargarPDFSeccion('primera','20211116', '/pdf/download_section')")
+driver.execute_script("descargarPDFSeccion('primera','20211118', '/pdf/download_section')")
+
 
 
