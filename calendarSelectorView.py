7fed403336e9ac6e1db2c5fa5aba253451f2014d
Juani Gerarduzzi <jigerarduzzi@gmail.com>
2021-11-29 11:25:16 -0800

    improved flow UI

--- calendarSelectorView.py
+++ calendarSelectorView.py
@@ -5,61 +5,70 @@ from datetime import datetime
 from downloader import Downloader
 from  tkinter import ttk
 
-selected_dates = []
+class CalendarSelector(object):
 
-root = Tk()
-root.title('Seleccione las BORAS que desea descargar')
-root.geometry("600x500")
+    def __init__(self):
+        super(CalendarSelector, self).__init__()
+        self.iniciarInterfaz()
+        pass
 
-#Set current date and set default selected date
-present = datetime.now()
-cal = Calendar(root, selectmode='day', year=int(present.strftime("%Y")), month=int(present.strftime("%m")), day=int(present.strftime("%d")), date_pattern="d/m/Y")
-cal.pack(pady=20)
+    def iniciarInterfaz(self):
 
-#Handle dates
-def grab_date():
-    selected_date = cal.get_date()
-    can_be_added = True
-    aux=datetime.strptime(str(selected_date), "%d/%m/%Y")
+        selected_dates = []
 
-    #Verify if selected date is not weekend
-    if (aux.weekday() < 5):
-        alert_weekend_label.config(text="")
-    else: 
-        alert_weekend_label.config(text="*fin de semana")
-        can_be_added = False
+        root = Tk()
+        root.title('Seleccione las BORAS que desea descargar')
+        root.geometry("600x500")
+
+        #Set current date and set default selected date
+        present = datetime.now()
+        cal = Calendar(root, selectmode='day', year=int(present.strftime("%Y")), month=int(present.strftime("%m")), day=int(present.strftime("%d")), date_pattern="d/m/Y")
+        cal.pack(pady=20)
+
+        #Handle dates
+        def grab_date():
+            selected_date = cal.get_date()
+            can_be_added = True
+            aux=datetime.strptime(str(selected_date), "%d/%m/%Y")
+
+        #Verify if selected date is not weekend
+            if (aux.weekday() < 5):
+                alert_weekend_label.config(text="")
+            else: 
+                alert_weekend_label.config(text="*fin de semana")
+                can_be_added = False
 
     #Verify if selected date is not future date
-    if(datetime.strptime(str(selected_date), "%d/%m/%Y")<=datetime.strptime(present.strftime("%d-%m-%Y"),"%d-%m-%Y")):
-        alert_future_date_label.config(text="")
-    else: 
-        alert_future_date_label.config(text="*fecha futura") 
-        can_be_added = False
+            if(datetime.strptime(str(selected_date), "%d/%m/%Y")<=datetime.strptime(present.strftime("%d-%m-%Y"),"%d-%m-%Y")):
+                alert_future_date_label.config(text="")
+            else: 
+                alert_future_date_label.config(text="*fecha futura") 
+                can_be_added = False
 
     #Add to array if it is possible
-    if(can_be_added) and (selected_date not in selected_dates):
-        selected_dates.append(selected_date)
-        my_label.config(text=selected_dates)    
+            if(can_be_added) and (selected_date not in selected_dates):
+                selected_dates.append(selected_date)
+                my_label.config(text=selected_dates)    
 
-def handle_downloads():
-    if len(selected_dates) > 0 :
-        downloader = Downloader
-        downloader.downloadFiles(selected_dates)
+        def handle_downloads():
+            if len(selected_dates) > 0 :
+                downloader = Downloader
+                downloader.downloadFiles(selected_dates)
 
-alert_weekend_label=Label(root,text="")
-alert_weekend_label.pack(pady=10)
-alert_future_date_label=Label(root,text="")
-alert_future_date_label.pack(pady=10)
-
-
-select_date_button=Button (root, text="Seleccionar", command=grab_date)
-select_date_button.pack(pady=20)
-
-my_label=Label(root,text="")
-my_label.pack(pady=20)
+        alert_weekend_label=Label(root,text="")
+        alert_weekend_label.pack(pady=10)
+        alert_future_date_label=Label(root,text="")
+        alert_future_date_label.pack(pady=10)
 
 
-download_button=Button (root, text="Descargar", command=handle_downloads)
-download_button.pack(pady=20)
+        select_date_button=Button (root, text="Seleccionar", command=grab_date)
+        select_date_button.pack(pady=20)
 
-root.mainloop()
+        my_label=Label(root,text="")
+        my_label.pack(pady=20)
+
+
+        download_button=Button (root, text="Descargar", command=handle_downloads)
+        download_button.pack(pady=20)
+
+        root.mainloop()
