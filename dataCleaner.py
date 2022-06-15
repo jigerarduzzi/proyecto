7fe8bed01ce14a5a124c20ff409dd03de476ecf3
Gerarduzzi <juan.gerarduzzi@accenture.com>
2022-06-15 19:07:26 -0300

    trainingDataset - civil - first advances

--- dataCleaner.py
+++ dataCleaner.py
@@ -4,13 +4,18 @@ from unidecode import unidecode
 import re
 from nltk.corpus import stopwords
 from nltk import SnowballStemmer
-
 #pip3 install spacy==2.2.4
 import spacy
 #py -m pip install install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.2.0/en_core_web_sm-2.2.0.tar.gz
 # or:  py -m spacy download es_core_news_sm 
 nlp = spacy.load("es_core_news_sm")
 spanishstemmer=SnowballStemmer('spanish')
+
+import json
+with open('civilDatasetTraining.json', encoding="utf8") as f:
+  data = json.load(f)
+
+
 
 
 def tokenization(text):
@@ -37,11 +42,22 @@ def cleanText(line):
     #convertir linea a tokens de palabras
     return tokenization(cleanedText)
 
+def flattenArray(originalArray):
+    flatArray=[]
+    for a in originalArray:
+        for item in a:
+            if(item!=" "):
+                flatArray.append(item)
+    
+    return flatArray
+
 with open('lawExampleToClean.txt',"r", encoding="utf8") as f:
     contents = f.readlines()
 
 #print(doc)
 
+
+cleanedLaw=[]
 for i in range(len(contents)):
     #print(unidecode(contents[i]).lower().replace("  ", " ").rstrip ( '\n'))#elimina caracteres especiales, mayusculas, espacios y saltos de linea repetidos
     #print(re.sub(r"(^artículo [0-9]*.-)|(^[a-z]*\))","",contents[i].lower()))
@@ -54,8 +70,15 @@ for i in range(len(contents)):
     #print(re.sub(r"(^articulo [0-9]*.-)|(^[a-z]*\))","",unidecode(lineWithoutStopWords.replace("  ", " ").rstrip ( '\n'))))
 
     ##print(stemming(tokenization(contents[i])))
+
     if(contents[i]!="\n"):
-        print(stemming(cleanText(contents[i])))
+        cleanedLaw.append(stemming(cleanText(contents[i])))
+
+#print(flattenArray(cleanedLaw))
+#print(stemming(cleanText(data[0]["contenido"])))
+
+for ley in range(len(data)):
+    print(stemming(cleanText(data[ley]["contenido"])))
     
 
 
