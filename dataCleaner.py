354178052c7fbce51b663004b0e1d5698f543247
Gerarduzzi <juan.gerarduzzi@accenture.com>
2022-06-27 15:59:30 -0300

    create datatraining automation - adjust regular expresions

--- dataCleaner.py
+++ dataCleaner.py
@@ -4,6 +4,7 @@ from unidecode import unidecode
 import re
 from nltk.corpus import stopwords
 from nltk import SnowballStemmer
+import json
 #pip3 install spacy==2.2.4
 import spacy
 #py -m pip install install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.2.0/en_core_web_sm-2.2.0.tar.gz
@@ -11,17 +12,9 @@ import spacy
 nlp = spacy.load("es_core_news_sm")
 spanishstemmer=SnowballStemmer('spanish')
 
-import json
-with open('comercialDatasetTraining.json', encoding="utf8") as f:
-  data = json.load(f)
-
-
-
-
 def tokenization(text):
     doc=nlp(text) #create spacy object of npl tipe
     return [tok.lemma_.lower() for tok in doc if tok.pos_ != 'PRON'] #crea lista de palabras del texto y aplica lemas
-
 
 def deleteStopwords(text, stopwords):
     return ' '.join([word for word in text.split(' ') if word not in stopwords])
@@ -31,10 +24,14 @@ def stemming(tokenLine): #convertir palabras a raices
     return stems
 
 def cleanText(line):
+    #agregar espacios donde hay saltos de lineas
+    cleanedText=re.sub(r"(\n)"," ",line)
     #eliminar saltos, caracteres especioales y espacios innecesarios
-    cleanedText=unidecode(line.lower().replace("  ", " ").rstrip ( '\n'))
+    cleanedText=re.sub("\s\s+", ' ',cleanedText)
+    cleanedText=unidecode(cleanedText)
     #eliminar articulo x y nro) del inicio de cada linea
-    cleanedText=re.sub(r"(^articulo [0-9]*.-)|(^[a-z]*\))","",cleanedText)
+    #print(cleanedText)
+    cleanedText=re.sub(r"(articulo [0-9]*)|(ley [0-9]*)|(^texto definitivo)|(ley e[0-9])|(antes dnu [0-9])|(antes ley [0-9])|(sancion: \d{2}/\d{2}/\d{4})|(publicacion: b.o. \d{2}/\d{2}/\d{4})|(actualizacion: \d{2}/\d{2}/\d{4})|(promulgacion: \d{2}/\d{2}/\d{4})|(rama: [a-z])|(^[a-z]*\))","",cleanedText.lower())
     #eliminar puntos y comas
     cleanedText=re.sub(r'[^\w\s]','',cleanedText)
     #eliminar stopwords
@@ -54,31 +51,18 @@ def flattenArray(originalArray):
 with open('lawExampleToClean.txt',"r", encoding="utf8") as f:
     contents = f.readlines()
 
-#print(doc)
-
-
 cleanedLaw=[]
 for i in range(len(contents)):
-    #print(unidecode(contents[i]).lower().replace("  ", " ").rstrip ( '\n'))#elimina caracteres especiales, mayusculas, espacios y saltos de linea repetidos
-    #print(re.sub(r"(^artículo [0-9]*.-)|(^[a-z]*\))","",contents[i].lower()))
-    #elimina caracteres especiales, mayusculas, espacios y saltos de linea repetidos
-    #expresion regular elimina lo que empieza con esos patrones (ejemplo, ARTÍCULO 26.- y a))
-   # print(re.sub(r"(^articulo [0-9]*.-)|(^[a-z]*\))","",unidecode(contents[i]).lower().replace("  ", " ").rstrip ( '\n')))
-    
-    
-    #lineWithoutStopWords=delete_stopwords(contents[i].lower(), stopwords.words('spanish'))
-    #print(re.sub(r"(^articulo [0-9]*.-)|(^[a-z]*\))","",unidecode(lineWithoutStopWords.replace("  ", " ").rstrip ( '\n'))))
-
-    ##print(stemming(tokenization(contents[i])))
-
     if(contents[i]!="\n"):
         cleanedLaw.append(stemming(cleanText(contents[i])))
 
-#print(flattenArray(cleanedLaw))
-#print(stemming(cleanText(data[0]["contenido"])))
+def cleanDataset(rama):
+    with open('./datasets/'+rama+'.json', encoding="utf8") as f:
+        data = json.load(f)
+    for ley in range(len(data)):
+        print(stemming(cleanText(data[ley]["contenido"])))
+        #cleanText(data[ley]["contenido"])
 
-for ley in range(len(data)):
-    print(stemming(cleanText(data[ley]["contenido"])))
     
 
 
--- dataExtractor.py
+++ dataExtractor.py
@@ -2,12 +2,16 @@
 from tika import parser   
 import glob
 import dataCreator
+import dataCleaner
 
 etiqueta = input('¿Qué dataset desea crear?')
 files = glob.glob("./Leyes/"+etiqueta+"/*.pdf")
 laws = []
 newData = dataCreator
+cleaner = dataCleaner
 
-for file in files:
-    parsed_pdf = parser.from_file(file) 
-    newData.addDataset(parsed_pdf['content'], etiqueta)
+#for file in files: descomentar para crear nuevos datasets
+#    parsed_pdf = parser.from_file(file) 
+#    newData.addDataset(parsed_pdf['content'], etiqueta)
+
+cleaner.cleanDataset(etiqueta)
